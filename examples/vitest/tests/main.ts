c3955d1ebbb5141824975384d23ace5a54409865
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-23 17:08:58 +0100

    Import testing utilities from Vitest

--- examples/vitest/tests/main.ts
+++ examples/vitest/tests/main.ts
@@ -1,4 +1,5 @@
-import assert from "assert";
+import { assert, describe, it } from 'vitest';
+
 
 describe(".example-template", function () {
   it("package.json has correct name", async function () {
